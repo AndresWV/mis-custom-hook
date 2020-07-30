96294e3e73cf7faa880818925d8e5cd59254d06e
AndresWV <andres.wv@hotmail.com>
2020-07-30 19:32:35 -0400

    IZI

---
+++ useFetch.js
@@ -0,0 +1,35 @@
+import {useState, useEffect, useRef} from 'react'
+
+export const useFetch = (url) => {
+    
+    const [state, setstate] = useState({data:null , loading: true , error: null})
+    const isMounted = useRef(true);
+
+    useEffect(() =>{
+        return () =>{
+            isMounted.current = false;
+        }
+    }, [])
+
+    useEffect( () => {
+        
+        setstate({data:null, loading:true, error:null});
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+            
+                if(isMounted.current){
+                    setstate({
+                        loading: false,
+                        error: null,
+                        data
+                    })
+                }
+            
+                
+            })
+    }, [url])   
+
+    return state;
+}
